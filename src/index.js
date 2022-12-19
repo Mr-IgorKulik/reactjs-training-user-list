d392ecb514355aa60393174ef976244d3ce2d579
Igor Kulik <mr.igor.kulik@gmail.com>
2022-12-19 20:09:48 +0200

    lesson3 - JSX in React JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from 'react'
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client"
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = () => console.log("Mouse is over")
+const helpText = "HELP TEXT"
+
+const elements = (<div className='name'>
+    <h1>{helpText}</h1>
+    <input placeholder={helpText} 
+      onClick={inputClick} onMouseOver={mouseOver}/>
+    <p>{helpText === "help" ? "Yes" : "No"}</p>
+</div>)
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"))
+root.render(elements)
