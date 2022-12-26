4b55b958da6e850f3b71d28121a43782b1148c05
Igor Kulik <mr.igor.kulik@gmail.com>
2022-12-26 16:26:37 +0200

    Add default values for user editing

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -17,12 +17,12 @@ class AddUser extends React.Component {
     render() {
         return (
             <form ref={(el) => this.addForm = el}>
-                <input placeholder='First Name' onChange={(e) => this.setState({ firstname: e.target.value })} />
-                <input placeholder='Second Name' onChange={(e) => this.setState({ secondname: e.target.value })} />
-                <textarea placeholder='Your bio' onChange={(e) => this.setState({ bio: e.target.value })}></textarea>
-                <input placeholder='Age' onChange={(e) => this.setState({ age: e.target.value })} />
+                <input placeholder='First Name' value={this.props.isEdit ? this.props.user.firstname : ''} onChange={(e) => this.setState({ firstname: e.target.value })} />
+                <input placeholder='Second Name' value={this.props.isEdit ? this.props.user.secondname : ''} onChange={(e) => this.setState({ secondname: e.target.value })} />
+                <textarea placeholder='Your bio' value={this.props.isEdit ? this.props.user.bio : ''} onChange={(e) => this.setState({ bio: e.target.value })}></textarea>
+                <input placeholder='Age' value={this.props.isEdit ? this.props.user.age : ''} onChange={(e) => this.setState({ age: e.target.value })} />
                 <label htmlFor="isHappy">Are you happy?</label>
-                <input type="checkbox" id='isHappy' onChange={(e) => this.setState({ isHappy: e.target.checked })} />
+                <input type="checkbox" id='isHappy' checked={this.props.isEdit ? this.props.user.isHappy : false} onChange={(e) => this.setState({ isHappy: e.target.checked })} />
                 <button type='button' onClick={() => {
                     this.addForm.reset()
                     this.userAdd = {
